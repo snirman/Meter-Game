840e2156f821c1c306531b69c178d2983eb90460
snirman <snir@magnifica.io>
2023-03-08 10:27:23 +0200

    :sparkles: feat(script): add methods in mover.cs

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -9,16 +9,30 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     
+        PrintInstrucion();
+
     }
 
     // Update is called once per frame
     void Update()
+    {  
+        MovePlayer();
+
+    }
+    void PrintInstrucion()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("This is Meter, he hates delivery guys");
+        Debug.Log("Move Meter with WASD or arrow keys");
+        Debug.Log("Don't hit the Walls!");
+        
+    }
+
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal")*Time.deltaTime*moveSpeed;
         float yValue = Input.GetAxis("Vertical")*Time.deltaTime*moveSpeed;
-
         transform.Translate(xValue,yValue,0);
-
     }
+
 }
